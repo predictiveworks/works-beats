dabb25ea65696231b9017666f7efe90903714dcc
Predictive Works <info@predictiveworks.eu>
2022-06-25 13:25:09 +0200

    Zeek project updated.

--- beats-zeek/src/main/scala/de/kp/works/beats/zeek/ZeekService.scala
+++ beats-zeek/src/main/scala/de/kp/works/beats/zeek/ZeekService.scala
@@ -1,6 +1,6 @@
 package de.kp.works.beats.zeek
-/*
- * Copyright (c) 2020 Dr. Krusche & Partner PartG. All rights reserved.
+/**
+ * Copyright (c) 2020 - 2022 Dr. Krusche & Partner PartG. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -18,10 +18,7 @@ package de.kp.works.beats.zeek
  *
  */
 
-import akka.NotUsed
-import akka.http.scaladsl.model.sse.ServerSentEvent
-import akka.http.scaladsl.server.Route
-import akka.stream.scaladsl.{Source, SourceQueueWithComplete}
+import akka.stream.scaladsl.SourceQueueWithComplete
 import com.typesafe.config.Config
 import de.kp.works.beats.file.FileMonitor
 import de.kp.works.beats.handler.OutputHandler
