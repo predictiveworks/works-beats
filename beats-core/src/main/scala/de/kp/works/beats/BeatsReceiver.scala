cd8445bf703220665fb215a97cc2c27abcbfc8de
Predictive Works <info@predictiveworks.eu>
2022-07-04 20:00:50 +0200

    Core project updated.

---
+++ beats-core/src/main/scala/de/kp/works/beats/BeatsReceiver.scala
@@ -0,0 +1,50 @@
+package de.kp.works.beats
+
+/**
+ * Copyright (c) 2020 - 2022 Dr. Krusche & Partner PartG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author Stefan Krusche, Dr. Krusche & Partner PartG
+ *
+ */
+
+import java.util.concurrent.{ExecutorService, Executors}
+
+abstract class BeatsReceiver(numThreads:Int = 1) extends BeatsLogging {
+
+  private var executorService:ExecutorService = _
+
+  def getWorker:Runnable
+
+  def start():Unit = {
+
+    try {
+
+      executorService = Executors.newFixedThreadPool(numThreads)
+      executorService.execute(getWorker)
+
+    } catch {
+      case _:Throwable => executorService.shutdown()
+    }
+
+  }
+
+  def stop():Unit = {
+
+    executorService.shutdown()
+    executorService.shutdownNow()
+
+  }
+
+}
